e9e74b1813fb0c1d49ecce5dd6c7f49868086a7c
fool160821 <s3650959@rmit.edu.vn>
2020-07-24 03:52:17 +0700

    Alternate approach to task A

--- animatedEmoji.py
+++ animatedEmoji.py
@@ -5,20 +5,33 @@ from emojis.frown import Frown
 from emojis.neutral import Neutral
 
 class AnimatedEmoji:
-    def animatedEmoji():
-        red = (255, 0, 0)
-        yellow = (255, 255, 0)
-        green = (0, 255, 0)
+    
+    def __init__(self, color1, color2, color3):
+        self.__color1 = color1
+        self.__color2 = color2
+        self.__color3 = color3
+
+    def aEmoji(self):
+        sense = SenseHat()
+
+        color1 = self.__color1
+        color2 = self.__color2
+        color3 = self.__color3
 
         emojis = [
-            Smile(green),
-            Neutral(yellow),
-            Frown(red)
+            Smile(color1),
+            Neutral(color2),
+            Frown(color3)
         ]
 
-    while True:
-        for emoji in emojis:
-            sense.set_pixels(emoji.face())
-            sleep(3)
+        while True:
+            for emoji in emojis:
+                sense.set_pixels(emoji.face())
+                sleep(3)
 
-AnimatedEmoji.animatedEmoji()
+r = (255, 0, 0)
+y = (255, 255, 0)
+g = (0, 255, 0)
+
+e1 = AnimatedEmoji(g, y, r)
+e1.aEmoji() 
